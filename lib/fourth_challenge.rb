784346cbaa6d7ed677d0bde165f343d5fe5bb66d
Patrick Alexander <patrick_alexander@mail.com>
2019-09-03 18:01:10 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,6 +7,8 @@ def fourth_challenge
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude: "worried"}
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      
+      
       ]
    }, 
    :capulet => {
